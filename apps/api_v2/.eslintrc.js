b279c655ea074cddcfae6d8ebf4967c8d7d2afd1
adityamandal-Developer <mandaladitya901@gmail.com>
2025-01-30 20:49:49 +0530

    completed backend in version 2 (api_v2)

--- apps/api_v2/.eslintrc.js
+++ apps/api_v2/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { semi: false, "endOfLine": "auto" }],
-    'eslint-disable-next-line prettier/prettier': 'off'
+    'prettier/prettier': ['error', { semi: false, endOfLine: 'auto' }],
+    'eslint-disable-next-line prettier/prettier': 'off',
   },
 }
